759e0fb972ddcadc5cf8d6e33111a814ca011262
sonodinn <srgr25@naver.com>
2025-11-03 19:28:28 +0900

    first

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def divdide (self, a, b):
+        if b == 0:
+            raise ValueError("0으로 나눌수 없습니다.")
+        return a / b
+    
